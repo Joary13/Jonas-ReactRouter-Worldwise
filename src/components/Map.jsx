50ba29bfe50b36a740fee999bbc5902d7e93031b
Joary13 <jolea13008@gmail.com>
2024-01-27 09:05:56 +0100

    nested Route app/

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  return (
+    <div className={styles.mapContainer}>
+      <Link to='form'>Map</Link>
+    </div>
+  );
 }
 
 export default Map;

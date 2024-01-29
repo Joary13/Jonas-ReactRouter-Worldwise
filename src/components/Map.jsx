10f57d325f42b22492788f3068b79dd09e1719df
Joary13 <jolea13008@gmail.com>
2024-01-29 10:41:03 +0100

    reading and setting query string

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
+/*eslint-disable*/
+
 function Map() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
   return (
     <div className={styles.mapContainer}>
       <Link to='form'>Map</Link>
+      <h1>Map</h1>
+      <h1>
+        Position {lat} {lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 50, lng: 12 })}>
+        click for change
+      </button>
     </div>
   );
 }
